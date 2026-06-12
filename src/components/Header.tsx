7c6eaf1145e0b8f43538e575e573540f4e0f8f35
NowScott <nowscott@qq.com>
2026-06-13 00:41:04 +0800

    feat: optimize desktop single-screen layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -64,7 +64,7 @@ export function Header({
           type="button"
           onClick={onOpenChangelog}
         >
-          v2.3.2
+          v2.4.0
         </button>
       </div>
     </header>
